3b04f6bf2f5c1a1d9e83c56adf3126980bbb7906
ktmymt <kota.miyamoto18@gmail.com>
2022-08-22 08:06:30 +0900

    [update]config

--- config/env/production/database.ts
+++ config/env/production/database.ts
@@ -1,7 +1,7 @@
-const parse = require('pg-connection-string').parse;
+import parse = require('pg-connection-string').parse;
 const config = parse(process.env.DATABASE_URL);
 
-module.exports = ({ env }) => ({
+export default ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
